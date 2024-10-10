bc998d48ca940736536c813b0a1199b009af0567
M Suraj Elijah <suraj.elijah@gmail.com>
2024-10-10 18:27:27 +0530

    Added the main Problems in Stacks Revised

--- Stacks_Queues/Stacks/9. Rain_Water_Trapping.cpp
+++ Stacks_Queues/Stacks/9. Rain_Water_Trapping.cpp
@@ -3,3 +3,12 @@
 //https://www.youtube.com/watch?v=FbGG2qpNp4U&list=PL_z_8CaSLPWdeOezg68SKkeLN4-T_jNHd&index=9
 
 // - Definite Watch
+
+
+/*
+
+The concept of NGL,NGR,NSL,NSR won't work here because we are trying to find not the nearest but the farthest
+one, but it is similar to how we found NGR and all, but not exactly using stack but it is Nice question
+
+
+*/
